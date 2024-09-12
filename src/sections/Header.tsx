50320afeabe2a2e817b887f9543399b775c1b2c8
Perspicacious <johnayomide920@gmail.com>
2024-09-12 14:17:55 +0100

    hero done

---
+++ src/sections/Header.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export const Header = () => {
+  return (
+  <div className="flex justify-center items-center fixed top-3 w-full Z-10">
+
+    {/*Navbar*/}
+    <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop:blur cursor-pointer">
+      <Link href={'#'} className="nav-item cursor-pointer">
+      Home
+      </Link>
+      <Link href={'#'} className="nav-item cursor-pointer">
+      Projects
+      </Link>
+      <Link href={'#'} className="nav-item cursor-pointer">
+      About
+      </Link>
+      <Link href={'#'} className="nav-item cursor-pointer bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">
+      Contact
+      </Link>
+    </nav>
+  </div>
+  );
+};
