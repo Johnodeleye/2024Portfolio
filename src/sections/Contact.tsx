7c6e0ee10e13f45130940c895f3538c12854c511
Perspicacious <johnayomide920@gmail.com>
2024-11-10 01:45:12 +0100

    ok

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -11,7 +11,7 @@ export const ContactSection = () => {
     setShowPopup(true);
   };
 
-  const onsubmit = async function handleSubmit(event: any) {
+  const onSubmit = async (event:any) => {
     event.preventDefault();
     const formData = new FormData(event.target);
 
@@ -19,28 +19,27 @@ export const ContactSection = () => {
 
     const object = Object.fromEntries(formData);
     const json = JSON.stringify(object);
-    const response = await fetch("https://api.web3forms.com/submit", {
+
+    const res = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json"
       },
       body: json
-    });
-    const result = await response.json();
-    if (result.success) {
+    }).then((res) => res.json());
+
+    if (res.success) {
+      // console.log("Success", res);
       Swal.fire({
-        title: "Success!",
-        text: "Thank you for your message, we'll get back to you!",
-        icon: "success"
+        icon:'success',
+        title: 'Thank you!',
+        text: 'Your message has been sent successfully.',
+        showConfirmButton: true,
       });
-    //   sendEmail();
-    console.log('Message Sent');
-    
-    }else{
-      console.log('error', Error)
+      event.target.reset();
     }
-  }
+  };
 
   return (
   <div className='py-16 pt-12 lg:py-24 lg:pt-20'>
@@ -68,11 +67,11 @@ export const ContactSection = () => {
             {showPopup && (
   <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center">
     <div className="bg-white rounded-md p-4 md:p-6 lg:p-8 w-11/12 md:w-2/3 lg:w-1/2 xl:w-1/3 z-50 relative">
-      <h2 className="text-2xl font-bold mb-4">Contact Us</h2>
-      <form action="#" className="flex flex-col gap-4" onSubmit={onsubmit}>
-        <input type="email" placeholder="Your email" className="p-2 border rounded-md" required/>
-        <input type="text" placeholder="Subject" className="p-2 border rounded-md" required/>
-        <textarea placeholder="Message" rows={4} className="p-2 border rounded-md" required></textarea>
+      <h2 className="text-2xl font-bold mb-4">Contact Form</h2>
+      <form onSubmit={onSubmit} className="flex flex-col gap-4">
+        <input type="email" placeholder="Your email" className="p-2 border rounded-md" required name='email'/>
+        <input type="text" placeholder="Your name" className="p-2 border rounded-md" required name='Name'/>
+        <textarea placeholder="Message" rows={4} className="p-2 border rounded-md" required name='message'></textarea>
         <button type="submit" className="bg-blue-700 text-white p-2 rounded-md hover:bg-blue-800">Send message</button>
       </form>
       <button className="text-red-600 font-bold absolute top-4 right-4" onClick={() => setShowPopup(false)}>×</button>
@@ -88,3 +87,46 @@ export const ContactSection = () => {
   );
 };
 
+// 'use client'
+// import React from "react";
+
+// export const ContactSection = () => {
+//   const onSubmit = async (event:any) => {
+//     event.preventDefault();
+//     const formData = new FormData(event.target);
+
+//     formData.append("access_key", "8d4a9798-6b50-4e7e-b2d6-55108dd78b15");
+
+//     const object = Object.fromEntries(formData);
+//     const json = JSON.stringify(object);
+
+//     const res = await fetch("https://api.web3forms.com/submit", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Accept: "application/json"
+//       },
+//       body: json
+//     }).then((res) => res.json());
+
+//     if (res.success) {
+//       console.log("Success", res);
+//     }
+//   };
+
+//   return (
+//       <form onSubmit={onSubmit} className="flex flex-col gap-4">
+//          <input type="email" placeholder="Your email" className="p-2 border rounded-md" required/>
+//          <input type="text" placeholder="Subject" className="p-2 border rounded-md" required/>
+//         <textarea name="message"></textarea>
+//         <button type="submit">Submit Form</button>
+//       </form>
+
+// // <form onSubmit={onSubmit} >
+// //
+// // 
+// // <textarea placeholder="Message" rows={4} className="p-2 border rounded-md" required></textarea>
+// // <button type="submit" className="bg-blue-700 text-white p-2 rounded-md hover:bg-blue-800">Send message</button>
+// // </form>
+//   );
+// }
